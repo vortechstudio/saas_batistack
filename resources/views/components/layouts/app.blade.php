841fab7ec20cf2ed66d8f8a69da51fe34843c725
Mockelyn Maxime <contact@vortechstudio.fr>
2025-08-25 15:39:14 +0200

    feat(layout): implement maryUI components and styles
    - Add maryUI styles and components to app layout
    - Include daisyUI plugin and custom pagination styles
    - Restructure app layout with mary-nav and mary-main components

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,28 +1,61 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8" />
-
-        <meta name="application-name" content="{{ config('app.name') }}" />
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <title>{{ config('app.name') }}</title>
-
-        <style>
-            [x-cloak] {
-                display: none !important;
-            }
-        </style>
-
-        @filamentStyles
-        @vite('resources/css/app.css')
+        @include('partials.head')
     </head>
 
-    <body class="antialiased">
-        {{ $slot }}
-
-        @livewire('notifications')
+    <body class="font-sans antialiased">
+        <x-mary-nav sticky full-width>
+            <x-slot:brand>
+                {{-- Drawer toggle for "main-drawer" --}}
+                <label for="main-drawer" class="lg:hidden mr-3">
+                    <x-icon name="heroicon-o-bars-3" class="cursor-pointer" />
+                </label>
+    
+                {{-- Brand --}}
+                <div>App</div>
+            </x-slot:brand>
+    
+            {{-- Right side actions --}}
+            <x-slot:actions>
+                <x-mary-button label="Messages" icon="o-envelope" link="###" class="btn-ghost btn-sm" responsive />
+                <x-mary-button label="Notifications" icon="o-bell" link="###" class="btn-ghost btn-sm" responsive />
+            </x-slot:actions>
+        </x-mary-nav>
+        <x-mary-main with-nav full-width>
+ 
+            {{-- This is a sidebar that works also as a drawer on small screens --}}
+            {{-- Notice the `main-drawer` reference here --}}
+            <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-200">
+    
+                {{-- User --}}
+                @if($user = auth()->user())
+                    <x-mary-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="pt-2">
+                        <x-slot:actions>
+                            <x-mary-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
+                        </x-slot:actions>
+                    </x-mary-list-item>
+    
+                    <x-mary-menu-separator />
+                @endif
+    
+                {{-- Activates the menu item when a route matches the `link` property --}}
+                <x-mary-menu activate-by-route>
+                    <x-mary-menu-item title="Home" icon="o-home" link="###" />
+                    <x-mary-menu-item title="Messages" icon="o-envelope" link="###" />
+                    <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth">
+                        <x-mary-menu-item title="Wifi" icon="o-wifi" link="####" />
+                        <x-mary-menu-item title="Archives" icon="o-archive-box" link="####" />
+                    </x-mary-menu-sub>
+                </x-mary-menu>
+            </x-slot:sidebar>
+    
+            {{-- The `$slot` goes here --}}
+            <x-slot:content>
+                {{ $slot }}
+                @livewire('notifications')
+            </x-slot:content>
+        </x-mary-main>
 
         @filamentScripts
         @vite('resources/js/app.js')
