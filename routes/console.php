fd69c09bdd3c8c74606c9c00ddeeadd316b0c615
Mockelyn Maxime <maximemockelyn8@gmail.com>
2025-10-25 22:36:25 +0200

    feat(monitoring): add service status check and error notification
    - Implement scheduled task to check customer service status every 5 minutes
    - Create ServiceError notification for mail, database and Slack channels
    - Update service status and notify customer when error is detected

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,35 @@
 <?php
 
+use App\Enum\Customer\CustomerServiceStatusEnum;
+use App\Models\Customer\CustomerService;
+use App\Notifications\Service\ServiceError;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    foreach (CustomerService::all() as $service) {
+        $request = Http::get("{$service->domain}/api/status");
+
+        $response = $request->json();
+
+        if($response['status'] !== 'ok'){
+            $service->update(['status' => CustomerServiceStatusEnum::ERROR]);
+        }
+
+        if ($response['status'] !== 'ok') {
+            $service->customer->user->notify(new ServiceError($service));
+        }
+    }
+})
+->everyFiveMinutes()
+->description("Vérifie l'état de chaque service Batistack et notifie l'utilisateur si nécessaire.");
+
+
+
+
