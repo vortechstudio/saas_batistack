85cd2ec00d737f64a72d124544d9d956173c2893
Mockelyn Maxime <maximemockelyn8@gmail.com>
2025-11-08 18:17:48 +0100

    feat(config): update Vite server configuration and clean up notification
    - Add host, port, and HMR configuration to `vite.config.js` for improved Hot Module Replacement (HMR) support.
    - Remove unused `ShouldQueue` import from `CreateSubscription` notification.

--- app/Notifications/Commerce/CreateSubscription.php
+++ app/Notifications/Commerce/CreateSubscription.php
@@ -4,7 +4,6 @@ namespace App\Notifications\Commerce;
 
 use App\Models\Commerce\Order;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Slack\SlackMessage;
 use Illuminate\Notifications\Notification;
--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,15 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            detectTls: 'saas.batistack.test',
         }),
         tailwindcss(),
     ],
     server: {
         cors: true,
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'saas.batistack.test', // Important pour le Hot Module Replacement (HMR)
+        }
     },
 });
